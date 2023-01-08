59b15b0df0a0ed58d4b3fb80aa84b0b56aa16662
hazimaiman <79648084+hazimaiman@users.noreply.github.com>
2023-01-08 19:46:35 +0800

    Create program.cs

---
+++ program.cs
@@ -0,0 +1,35 @@
+using System.Runtime.CompilerServices;
+
+namespace MyFirstProgram
+    {
+        class Program
+        {
+        static void Main(string[] args)
+        {
+            //array = a variable that can store multiple value. fixed size
+
+            String[] cars = new string[3];
+
+
+            //String[] cars = {"BWM" , "Mustang", "Myvi" };
+
+            cars[0] = "Tesla";
+            cars[1] = "Mustang";
+            cars[2] = "Corvette";
+
+            for (int i = 0; i < cars.Length; i++)
+            {
+                Console.WriteLine(cars[i]);
+            }
+
+
+            Console.ReadKey();
+            }
+        }
+    }
+
+
+
+
+
+   
